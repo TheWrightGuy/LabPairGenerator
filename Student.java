e5a580cdb7fc4ff5f0bebb60df58a7f7fb47b4da
TheWrightGuy <logan.drescher@trincoll.edu>
2019-04-21 17:50:04 -0400

    added

---
+++ Student.java
@@ -0,0 +1,8 @@
+/** 
+ * Defines the student object and associated operations
+ * 
+ * @author Logan C.W. Drescher
+ * @version 0.0.0
+ * Date Created:        April 21st, 2019
+ * Date Last Edited:    April 21st, 2019
+ */
